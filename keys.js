da0f109ffc326c00b04474af2857ff4c2eace2f0
Alan32575 <52725927+Alan32575@users.noreply.github.com>
2019-11-06 20:44:43 -0500

    getting the Spotify part up

--- keys.js
+++ keys.js
@@ -1,12 +1,4 @@
-console.log('this is loaded');
-
-
-var searchOMDB = function(movie) {
-    var queryURL = "https://www.omdbapi.com/?t=" + movie + fb8ec145;
-    $.ajax({
-      url: queryURL,
-      method: "GET"
-    }).then(function(response) {
-      createRow(response);
-    });
-  };
+exports.spotify = {
+  id: process.env.SPOTIFY_API_KEY,
+  secret: process.env.SPOTIFY_SECRET
+}
---
+++ liri.js
@@ -0,0 +1,25 @@
+require('dotenv').config()
+var keys = require('./keys')
+var Spotify = require('node-spotify-api');
+var spotify = new Spotify(keys.spotify);
+
+var command = process.argv[2];
+var searchTerm = process.argv.splice(3).join(" ");
+
+RunLiri(command, searchTerm);
+
+function RunLiri(func, find){
+    if(func === "spotify-this-song"){
+        return SearchSpotify(find)
+    }
+}
+
+function SearchSpotify(query){
+    spotify.search({ type: 'track', query }, function(err, data) {
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        }
+        
+        console.log(data.tracks.items[0].artists[0].name); 
+    });
+}
